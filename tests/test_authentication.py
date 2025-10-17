46c081026c2ce792f38e335eaed0b60b67352456
vshopin <vshopin@usergate.com>
2025-10-17 14:46:46 +0700

    Маркировки Pytest.Практика работы с pytest маркировками

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,12 +1,17 @@
 from http import HTTPStatus
-from clients.users.public_users_client import get_public_users_client
-from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
+
+import pytest
+
 from clients.authentication.authentication_client import get_authentication_client
+from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
+from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
-from tools.assertions.base import assert_status_code
 from tools.assertions.authentication import assert_login_response
+from tools.assertions.base import assert_status_code
 
 
+@pytest.mark.regression
+@pytest.mark.authentication
 def test_authentication_response():
     public_users_client = get_public_users_client()
     authentication_client = get_authentication_client()
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,12 +1,16 @@
 from http import HTTPStatus
 
+import pytest
+
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
-from tools.assertions.schema import validate_json_schema
 from tools.assertions.base import assert_status_code
+from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response
 
 
+@pytest.mark.users
+@pytest.mark.regression
 def test_create_user():
     public_users_client = get_public_users_client()
 
@@ -16,5 +20,5 @@ def test_create_user():
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
     assert_status_code(response.status_code, HTTPStatus.OK)
-    assert_create_user_response(request, response)
+    assert_create_user_response(request, response_data)
     validate_json_schema(response.json(), response_data.model_json_schema())
