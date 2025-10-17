6172dab8ef181e5985a13f5661a96c856e3f05c7
vshopin <shopin_va@mail.ru>
2025-10-17 20:38:10 +0700

    Фикстуры Pytest.Практика написания автотестов с использованием фикстур

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -32,11 +32,11 @@ def assert_user(actual: UserSchema, expected: UserSchema):
     :param expected: Ожидаемая структура данных пользователя.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
-    assert_equal(actual.user.id, expected.user.id, "Id")
-    assert_equal(actual.user.email, expected.user.email, "email")
-    assert_equal(actual.user.last_name, expected.user.last_name, "last_name")
-    assert_equal(actual.user.first_name, expected.user.first_name, "first_name")
-    assert_equal(actual.user.middle_name, expected.user.middle_name, "middle_name")
+    assert_equal(actual.id, expected.id, "Id")
+    assert_equal(actual.email, expected.email, "email")
+    assert_equal(actual.last_name, expected.last_name, "last_name")
+    assert_equal(actual.first_name, expected.first_name, "first_name")
+    assert_equal(actual.middle_name, expected.middle_name, "middle_name")
 
 
 def assert_get_user_response(
@@ -50,4 +50,4 @@ def assert_get_user_response(
     :param create_user_response: Ответ API при создании пользователя.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
-    assert_user(get_user_response, create_user_response)
+    assert_user(get_user_response.user, create_user_response.user)
