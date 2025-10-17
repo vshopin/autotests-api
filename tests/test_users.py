39c3d8a2ae5fb7ed3efcbf042708ae8019a71c50
vshopin <shopin_va@mail.ru>
2025-10-17 20:28:22 +0700

    Фикстуры Pytest.Практика написания автотестов с использованием фикстур

--- tests/test_users.py
+++ tests/test_users.py
@@ -31,8 +31,10 @@ def test_create_user(public_users_client: PublicUsersClient):
 @pytest.mark.users
 @pytest.mark.regression
 def test_get_user_me(private_users_client: PrivateUsersClient, function_user: UserFixture):
-    request = private_users_client.get_user_me_api()
-    request_data = GetUserResponseSchema.model_validate_json(request.text)
+    response = private_users_client.get_user_me_api()
+    response_data = GetUserResponseSchema.model_validate_json(response.text)
 
-    assert_status_code(request.status_code, HTTPStatus.OK)
-    assert_get_user_response(request_data, function_user.response)
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_get_user_response(response_data, function_user.response)
+
+    validate_json_schema(response.json(), response_data.model_json_schema())
