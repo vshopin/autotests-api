8c39f27742267de15361b591692bbed20cc73196
vshopin <shopin_va@mail.ru>
2025-11-02 14:18:07 +0700

    Практика написания API автотестов для работы с заданиями.Получение задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -6,10 +6,15 @@ from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import (
     CreateExerciseRequestSchema,
     CreateExerciseResponsetSchema,
+    GetExerciseResponseSchema,
 )
 from fixtures.courses import CourseFixture
+from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.exercises import assert_create_exercise_response
+from tools.assertions.exercises import (
+    assert_create_exercise_response,
+    assert_get_exercise_response,
+)
 from tools.assertions.schema import validate_json_schema
 
 
@@ -27,5 +32,18 @@ class TestExercises:
 
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_create_exercise_response(request, response_data)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_get_exercise(
+        self,
+        exercises_client: ExercisesClient,
+        function_exercise: ExerciseFixture,
+    ):
+        response = exercises_client.get_exercise_api(function_exercise.response.exercise.id)
+        response_data = GetExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_get_exercise_response(response_data, function_exercise.response)
 
         validate_json_schema(response.json(), response_data.model_json_schema())
--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,6 +1,8 @@
 from clients.exercises.exercises_schema import (
     CreateExerciseRequestSchema,
     CreateExerciseResponsetSchema,
+    ExerciseSchema,
+    GetExerciseResponseSchema,
 )
 from tools.assertions.base import assert_equal
 
@@ -22,3 +24,35 @@ def assert_create_exercise_response(
     assert_equal(response.exercise.order_index, request.order_index, "order_index")
     assert_equal(response.exercise.description, request.description, "description")
     assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
+
+
+def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
+    """
+    Проверяет, что фактические данные задания соответствуют ожидаемым.
+
+    :param actual: Фактические данные задания.
+    :param expected: Ожидаемые данные задания.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.title, expected.title, "title")
+    assert_equal(actual.course_id, expected.course_id, "course_id")
+    assert_equal(actual.max_score, expected.max_score, "max_score")
+    assert_equal(actual.min_score, expected.min_score, "min_score")
+    assert_equal(actual.order_index, expected.order_index, "order_index")
+    assert_equal(actual.description, expected.description, "description")
+    assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+
+
+def assert_get_exercise_response(
+    get_exercise_response: GetExerciseResponseSchema,
+    create_exercise_response: CreateExerciseResponsetSchema,
+):
+    """
+    Проверяет, что ответ на получение задания соответствует ответe на его создание.
+
+    :param get_exercise_response: Ответ API при запросе задания.
+    :param create_exercise_response: Ответ API при создании задания.
+    :raises AssertionError: Если данные задания не совпадают.
+    """
+    assert_exercise(get_exercise_response.exercise, create_exercise_response.exercise)
