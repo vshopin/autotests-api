20a19da01e30c64d0aa1ee760e253554dc5fb07e
vshopin <vshopin@users.noreply.github.com>
2026-07-01 05:04:20 +0000

    deploy: bf62a4f9cc92b88f352521f45b53afac523387ed

--- fixtures/allure.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from tools.allure.environment import create_allure_environment
-
-
-@pytest.fixture(scope='session', autouse=True)
-def save_allure_environment():
-    yield
-    create_allure_environment()
